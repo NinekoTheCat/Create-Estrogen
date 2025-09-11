548de5cb0b397384f7465a8950435078ac00959b
Niko Dale <cnotsomark@gmail.com>
2025-09-11 15:24:51 -0400

    feat: added an EstrogenModule implementation to fit in with estrogen

--- src/main/kotlin/dev/mayaqq/createestrogen/CreateEstrogen.kt
+++ src/main/kotlin/dev/mayaqq/createestrogen/CreateEstrogen.kt
@@ -2,7 +2,12 @@ package dev.mayaqq.createestrogen
 
 import dev.mayaqq.createestrogen.content.*
 import dev.mayaqq.cynosure.events.api.EventSubscriber
-import net.createmod.ponder.foundation.PonderIndex
+import dev.mayaqq.cynosure.utils.colors.Color
+import dev.mayaqq.estrogen.api.EstrogenEntrypoint
+import dev.mayaqq.estrogen.api.EstrogenFlag
+import dev.mayaqq.estrogen.api.EstrogenModule
+import dev.mayaqq.estrogen.client.content.screen.EstrogenMenuScreen
+import net.minecraft.client.gui.screens.Screen
 import net.minecraft.resources.ResourceLocation
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -12,8 +17,10 @@ const val MOD_ID = "createestrogen"
 const val MOD_NAME = "Create: Estrogen"
 internal inline fun id(path: String) = ResourceLocation(MOD_ID, path)
 
+@EstrogenEntrypoint
 @EventSubscriber
-object CreateEstrogen : Logger by LoggerFactory.getLogger(MOD_NAME),  RegistryManager by RegistryManager(MOD_ID) {
+object CreateEstrogen : Logger by LoggerFactory.getLogger(MOD_NAME), RegistryManager by RegistryManager(MOD_ID),
+    EstrogenModule {
     fun init() {
         CreateEstrogenRecipes.register()
         CreateEstrogenRecipes.Serializers.register()
@@ -22,4 +29,13 @@ object CreateEstrogen : Logger by LoggerFactory.getLogger(MOD_NAME),  RegistryMa
         CreateEstrogenItems.register()
         CreateEstrogenCreativeTab.register()
     }
+
+    override val color: Color
+        get() = Color(0xFFC7A7)
+    override val description: String
+        get() = "Create module for Estrogen"
+    override val flags: Array<EstrogenFlag>
+        get() = arrayOf(EstrogenFlag.DISABLES_CAULDRON_ESTROGEN)
+
+    override fun createConfigScreen(): (Screen) -> Screen = { EstrogenMenuScreen(it) }
 }
