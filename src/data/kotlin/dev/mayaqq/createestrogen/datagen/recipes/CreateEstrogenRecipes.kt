9615ac720c16418097f6bea0eac860ee3b41ee5e
Niko Dale <cnotsomark@gmail.com>
2026-08-01 17:57:42 -0400

    feat: fixed recipes being saved to Estrogen's ID instead of create: estrogen's

--- src/data/kotlin/dev/mayaqq/createestrogen/datagen/recipes/CreateEstrogenRecipes.kt
+++ src/data/kotlin/dev/mayaqq/createestrogen/datagen/recipes/CreateEstrogenRecipes.kt
@@ -3,6 +3,7 @@ package dev.mayaqq.createestrogen.datagen.recipes
 import com.simibubi.create.AllBlocks
 import com.simibubi.create.AllItems
 import dev.mayaqq.createestrogen.content.CreateEstrogenBlocks
+import dev.mayaqq.createestrogen.id
 import dev.mayaqq.cynosure.utils.tag
 import dev.mayaqq.estrogen.cid
 import dev.mayaqq.estrogen.content.EstrogenBlocks
@@ -10,11 +11,7 @@ import dev.mayaqq.estrogen.content.EstrogenItems
 import net.minecraft.core.HolderLookup
 import net.minecraft.core.registries.Registries
 import net.minecraft.data.PackOutput
-import net.minecraft.data.recipes.RecipeCategory
-import net.minecraft.data.recipes.RecipeOutput
-import net.minecraft.data.recipes.RecipeProvider
-import net.minecraft.data.recipes.ShapedRecipeBuilder
-import net.minecraft.data.recipes.ShapelessRecipeBuilder
+import net.minecraft.data.recipes.*
 import net.minecraft.tags.ItemTags
 import java.util.concurrent.CompletableFuture
 
@@ -31,7 +28,7 @@ class CreateEstrogenRecipes(output: PackOutput, lookup: CompletableFuture<Holder
             .unlockedBy(getHasName(EstrogenItems.EstrogenPill), has(EstrogenItems.EstrogenPill))
             .requires(AllItems.WHEAT_FLOUR)
             .requires(AllItems.BAR_OF_CHOCOLATE)
-            .save(this)
+            .save(this, id("estrogen_pill"))
         ShapedRecipeBuilder.shaped(RecipeCategory.REDSTONE, CreateEstrogenBlocks.Centrifuge, 1)
             .define('P', AllBlocks.MECHANICAL_PUMP)
             .define('T', AllBlocks.FLUID_TANK)
@@ -41,14 +38,14 @@ class CreateEstrogenRecipes(output: PackOutput, lookup: CompletableFuture<Holder
             .pattern("PMP")
             .pattern("CTC")
             .unlockedBy(getHasName(AllItems.PRECISION_MECHANISM), has(AllItems.PRECISION_MECHANISM))
-            .save(this)
+            .save(this, id("centrifuge"))
         ShapelessRecipeBuilder.shapeless(RecipeCategory.BUILDING_BLOCKS, CreateEstrogenBlocks.MothSeat)
             .requires(EstrogenBlocks.QuiltedMothWool.get(), 1)
             .requires(ItemTags.WOODEN_SLABS)
             .unlockedBy(
                 getHasName(EstrogenBlocks.MothWool.get()), has(EstrogenBlocks.MothWool.get())
             )
-            .save(this)
+            .save(this, id("moth_wool"))
         ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, EstrogenBlocks.CookieJar.get(), 1)
             .define('G', Registries.ITEM.tag(cid("glass_panes")))
             .define('Z', Registries.ITEM.tag(cid("nuggets/zinc")))
@@ -56,6 +53,6 @@ class CreateEstrogenRecipes(output: PackOutput, lookup: CompletableFuture<Holder
             .pattern("G G")
             .pattern("GGG")
             .unlockedBy(getHasName(AllItems.ZINC_NUGGET), has(AllItems.ZINC_NUGGET))
-            .save(this)
+            .save(this, id("cookie_jar"))
     }
 }
